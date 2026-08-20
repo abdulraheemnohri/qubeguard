4b7183e9b9df0494878bcc47c91b3f2e3201e969
abdulraheemnohri <31163557+abdulraheemnohri@users.noreply.github.com>
2026-08-20 22:23:57 +0500

    Add Hilt Navigation Compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,11 +27,11 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions { jvmTarget = "1.8" }
+    kotlinOptions { jvmTarget = "17" }
 
     buildFeatures {
         compose = true
@@ -56,6 +56,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling:1.5.3")
 
     implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")

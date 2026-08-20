07d5b6eeb87fe97e0570d058776022ff21bede26
abdulraheemnohri <31163557+abdulraheemnohri@users.noreply.github.com>
2026-08-20 10:38:46 +0500

    feat: Add project-level Gradle settings for QubeGuard

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android\\..*")
+                includeGroupByRegex("com\\.google\\..*")
+                includeGroupByRegex("androidx\\..*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android\\..*")
+                includeGroupByRegex("com\\.google\\..*")
+                includeGroupByRegex("androidx\\..*")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+rootProject.name = "QubeGuard"
+include(":app")
